0c62f84d37b2bcae89f2217b548ba158288268d5
organelleV <fitchw42@gmail.com>
2023-03-25 10:11:22 -0400

    moved key

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
-df = load_data(st.secrets["public_gsheets_url"])
+df = load_data("https://docs.google.com/spreadsheets/d/1bmMR23Jn9PabjIPN1M7wKAKC6qr4g8__IMF9_c2AmHI/edit?usp=sharing")
 
 # Print results.
 for row in df.itertuples():
